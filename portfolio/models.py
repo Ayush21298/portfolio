7538d2bbb50bdf808b06719c258559bff950375f
Ayush Patel <patel.ayush08@gmail.com>
2017-03-04 19:49:13 +0530

    Model Pers_Info

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
 
+
 from django.db import models
 
-# Create your models here.
+class Pers_Info(models.Model):
+	name = models.CharField(max_length=32)
+	entry = models.CharField(max_length=11)
+	branch = models.CharField(max_length=32)
+	dob = models.DateField("Date Of Birth")
+	Age = models.IntegerField(default=0)
+	contact_no = models.IntegerField(max_length=10)
+	contact_id = models.CharField(max_length=32)
